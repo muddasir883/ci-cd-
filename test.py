bf0a7f3be78068464554a27410496644e2028f2d
muddasir883 <162773176+muddasir883@users.noreply.github.com>
2024-04-21 01:53:06 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('test.html')  # Remove the absolute path
 #def index():
     # Get the absolute path of the directory containing this script
-if _name_ == '_main_':
+if __name__ == '_main_':
     app.run(debug=True, host='0.0.0.0', port=8000)
